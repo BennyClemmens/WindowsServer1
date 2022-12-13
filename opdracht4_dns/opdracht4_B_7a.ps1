a88276631d78f259ae5b553e10ae865d6a34e16c
Benny Clemmens <benny.clemmens@student.hogent.be>
2022-12-13 20:39:25 +0100

    remove zones

---
+++ opdracht4_dns/opdracht4_B_7a.ps1
@@ -0,0 +1,12 @@
+﻿<#
+    7. Verwijder via PowerShell de zone example.temp
+    en de reverse lookup zone voor netwerk 192.168.10.0/24.
+    Maak hierbij gebruik van de parameter -Force zodat de gebruiker geen
+    bevestiging moet geven bij uitvoeren van het script.
+#>
+
+$zonenaam = "example.temp"
+$reversezonenaam = "10.168.192.in-addr.arpa"
+
+Remove-DnsServerZone ${zonenaam} -Force
+Remove-DnsServerZone ${reversezonenaam} -Force
