5a924a8624c063ce3f4d50abd0f6d91984ef02df
Benny Clemmens <benny.clemmens@student.hogent.be>
2022-12-14 06:45:24 +0100

    OU zoeken

---
+++ opdracht5_ad/opdracht5_1b.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    1. Schrijf een script dat aan de gebruiker vraagt om de naam van OU in te geven (bijvoorbeeld: PersoneelGent).
+    Nadien schrijft het script voor elke OU met deze naam het volledige pad uit in X500 (LDAP) formaat.  
+#>
+
+#model oplossing
+
+$filter = read-host "geef de naam (filter) van een OU"
+
+$ous = Get-ADOrganizationalUnit -Filter {Name -like ${filter}}
+
+foreach($ou in ${ous})
+    {
+    write-host ${ou}.distinguishedname
+    }
