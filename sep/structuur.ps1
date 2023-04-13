b3d333cabe0e89e64d7a16df1b1ce221e20613bb
Benny <benny.clemmens@student.hogent.be>
2023-04-13 14:07:46 +0200

    sep

---
+++ sep/structuur.ps1
@@ -0,0 +1,18 @@
+﻿#structuur.csv
+
+$T01_PATH="DC=florist,DC=local"
+$T01_STRUCTUUR = Import-Csv "$(Split-Path $MyInvocation.MyCommand.Path -Parent)\csv\structuur.csv" -Delimiter ";"
+
+foreach ($T01_CONTAINER in ${T01_STRUCTUUR}){
+    [String]$T01_CONTAINERNAAM = ${T01_CONTAINER}.Containernaam;
+    [String]$T01_DELIM = ","
+    if (-not ${T01_CONTAINER}.Prepath) {
+        $T01_DELIM = ""
+    }
+    [String]$T01_VOLLEDIGPATH = ${T01_CONTAINER}.Prepath + ${T01_DELIM} + ${T01_PATH}
+    write-host ${T01_CONTAINERNAAM} wordt aangemaakt in ${T01_VOLLEDIGPATH}
+    New-ADOrganizationalUnit -Name ${T01_CONTAINERNAAM} -ProtectedFromAccidentalDeletion $false -Path ${T01_VOLLEDIGPATH}
+}
+
+
+# $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
---
+++ sep/testjes.ps1
@@ -0,0 +1,45 @@
+﻿#testje :)
+
+# Get-WindowsFeature -Name AD-Domain-Services
+
+# New-ADOrganizationalUnit -DisplayName
+
+
+# $domein="florist.local"
+$T01_PATH="DC=thematrix,dc=local"
+# New-ADOrganizationalUnit -Name DomainWorkstations
+
+
+##[String[]]$T01_OUS = "DomainWorstations", "DomainUsers"
+
+##Foreach(${T01_OU} in ${T01_OUS})  
+   {  
+         Write-Host ${T01_OU} wordt aangemaakt
+         New-ADOrganizationalUnit -Name ${T01_OU} -ProtectedFromAccidentalDeletion $false
+    }
+
+
+[String[]]$T01_WORKSTATIONS = "PCs", "Servers"
+
+$T01_PREPATH="OU=DomainWorstations"
+
+Foreach(${T01_OU} in ${T01_OUS})  
+   {  
+         Write-Host ${T01_OU} wordt aangemaakt
+         New-ADOrganizationalUnit -Name ${T01_OU} -ProtectedFromAccidentalDeletion $false -Path "${T01_PREPATH},${T01_PATH}"
+    }
+
+## aanmaken domein-structuur Active directory:
+
+New-ADOrganizationalUnit -Name "DomainWorkstations" -ProtectedFromAccidentalDeletion $false
+New-ADOrganizationalUnit -Name "PCs" -ProtectedFromAccidentalDeletion $false -Path "OU=Domainworkstations,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Servers" -ProtectedFromAccidentalDeletion $false -Path "OU=DomainWorkstations,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Cast" -ProtectedFromAccidentalDeletion $false -Path "OU=PCs,OU=DomainWorkstations,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Crew" -ProtectedFromAccidentalDeletion $false -Path "OU=PCs,OU=DomainWorkstations,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Directors" -ProtectedFromAccidentalDeletion $false -Path "OU=Crew,OU=PCs,OU=DomainWorkstations,DC=TheMatrix,DC=Local"
+
+New-ADOrganizationalUnit -Name "DomainUsers" -ProtectedFromAccidentalDeletion $false
+New-ADOrganizationalUnit -Name "Cast" -ProtectedFromAccidentalDeletion $false -Path "OU=DomainUsers,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Crew" -ProtectedFromAccidentalDeletion $false -Path "OU=DomainUsers,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Directors" -ProtectedFromAccidentalDeletion $false -Path "OU=Crew,OU=DomainUsers,DC=TheMatrix,DC=Local"
+New-ADOrganizationalUnit -Name "Producers" -ProtectedFromAccidentalDeletion $false -Path "OU=Crew,OU=DomainUsers,DC=TheMatrix,DC=Local"
